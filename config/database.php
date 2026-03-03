bbdc96a88d2fc5070c6bd22243d4182fbc8e86db
Kristian Hernandez <hkristianlloyd2@gmail.com>
2026-03-04 00:13:25 +0800

    Fixed undefined port property

--- config/database.php
+++ config/database.php
@@ -3,22 +3,33 @@
 // PDO connection
 
 class Database {
+
     private static $instance = null;
     private $conn;
 
-    private $host = 'localhost';
-    private $db   = 'ojt_tracker';
-    private $user = 'root';
-    private $pass = '';
+    private $host;
+    private $db;
+    private $user;
+    private $pass;
+    private $port; //  important
     private $charset = 'utf8mb4';
 
     private function __construct() {
-        $dsn = "mysql:host={$this->host};dbname={$this->db};charset={$this->charset}";
+
+        $this->host = getenv('MYSQLHOST') ?: 'localhost';
+        $this->db   = getenv('MYSQLDATABASE') ?: 'ojt_tracker';
+        $this->user = getenv('MYSQLUSER') ?: 'root';
+        $this->pass = getenv('MYSQLPASSWORD') ?: '';
+        $this->port = getenv('MYSQLPORT') ?: 3306;
+
+        $dsn = "mysql:host={$this->host};port={$this->port};dbname={$this->db};charset={$this->charset}";
+
         $opt = [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES   => false,
         ];
+
         $this->conn = new PDO($dsn, $this->user, $this->pass, $opt);
     }
 
@@ -26,6 +37,7 @@ class Database {
         if (self::$instance === null) {
             self::$instance = new Database();
         }
+
         return self::$instance->conn;
     }
 }
